4df9b6cb904c995aa868400004b18beec7ab45bf
Josue Vallejo <yosue4422@gmail.com>
2024-04-08 22:46:25 -0500

    add url

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -3,26 +3,32 @@ import store from '../reduxStore/store';
 import { showNotification } from '../shared/Notification/notificationSlice';
 
 const axios = axiosOriginal.create({
-    baseURL: import.meta.env.VITE_API_URL
-})
-
-axios.interceptors.request.use(function (config) {
-    const token = store.getState().auth.token;
-    if(config.headers?.Authorization) return config;
-    config.headers.Authorization =  `Bearer ${token}`;
-    return config;
+  //   baseURL: import.meta.env.VITE_API_URL,
+  baseURL: 'https://verification-app-back.onrender.com',
 });
 
-axios.interceptors.response.use((res) => {
+axios.interceptors.request.use(function (config) {
+  const token = store.getState().auth.token;
+  if (config.headers?.Authorization) return config;
+  config.headers.Authorization = `Bearer ${token}`;
+  return config;
+});
+
+axios.interceptors.response.use(
+  (res) => {
     return res;
-}, error => {
-    if(error.response?.status === 401) throw error;
-    store.dispatch(showNotification({
-        variant: "danger",
-        message: "There was an error"
-    }))
+  },
+  (error) => {
+    if (error.response?.status === 401) throw error;
+    store.dispatch(
+      showNotification({
+        variant: 'danger',
+        message: 'There was an error',
+      })
+    );
     console.log(error);
     throw error;
-})
+  }
+);
 
 export default axios;
